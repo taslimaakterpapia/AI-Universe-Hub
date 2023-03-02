2d973694a75ab4b5bd9bfd13d4ff0ed38387ac2e
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2023-03-03 03:13:21 +0600

    url js console log

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-
+const loadData = () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    console.log(url)
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displaycard(data.data.tools));
+  
+};
 
 
 loadData();
